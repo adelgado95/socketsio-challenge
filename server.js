49da8d8d1d3cfe575f0f1c286c76ec83c7ca82b5
Alfonso Delgado <ajdfdelgado@gmail.com>
2022-08-04 16:24:59 -0600

    Initial setup

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const http = require("http").createServer();
+const io = require("socket.io")(http);
+//Listen for a client connection 
+io.on("connection", (socket) => {
+    //Socket is a Link to the Client 
+    console.log("New Client is Connected!");
+    //Here the client is connected and we can exchanged 
+    socket.emit("welcome", "Hello and Welcome to the Server");
+});
+
+//Listen the HTTP Server 
+http.listen(port, () => {
+    console.log("Server Is Running Port: " + port);
+});
